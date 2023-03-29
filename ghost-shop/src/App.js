0bfa78e191a28c42b449ee47fee1947938cbf88c
harshal-kitukale <harshalkitukale005@gmail.com>
2023-03-29 21:45:27 +0530

    added single product page code

--- ghost-shop/src/App.js
+++ ghost-shop/src/App.js
@@ -1,13 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
-import { ItemDetails } from './harshal/SingleProduct';
-
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
-      <ItemDetails/>
     </div>
   );
 }
