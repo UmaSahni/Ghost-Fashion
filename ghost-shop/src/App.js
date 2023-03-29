12fe66a14e64a3da81d95fccaeab9131bc5dbd5c
VinitAsutkar <vinit123asutkar@gmail.com>
2023-03-29 21:37:22 +0530

    Navbar

--- ghost-shop/src/App.js
+++ ghost-shop/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <Navbar/>
     </div>
   );
 }
