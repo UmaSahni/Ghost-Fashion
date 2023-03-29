9769fa6b102a4d70aa56668a3751f2b825d227a8
unknown <1802.nehasahu@gmail.com>
2023-03-29 21:39:24 +0530

    day2

--- ghost-shop/src/App.js
+++ ghost-shop/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import {ProductPage} from './components/ProductPage';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome</h1>
+      <ProductPage/>
     </div>
   );
 }
