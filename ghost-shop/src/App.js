abbf195ccb52e9be4dede2d1ead70d652256041a
harshal-kitukale <harshalkitukale005@gmail.com>
2023-03-30 20:04:43 +0530

    resolve errors

--- ghost-shop/src/App.js
+++ ghost-shop/src/App.js
@@ -1,8 +1,6 @@
 import AllRoutes from './AllRoutes/AllRoutes';
 import './App.css';
 import Footer from './Components/Footer';
-import Navbar from './Components/Navbar';
-// import { ProductPage } from './neha/ProductPage';
 function App() {  
   return (
     <div className="App">
