adead8b86fabc1d56233b3112c3a91c82359d073
harshal-kitukale <harshalkitukale005@gmail.com>
2023-03-30 19:33:40 +0530

    added some changes of single product

--- ghost-shop/src/App.js
+++ ghost-shop/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-import { ProductPage } from './neha/ProductPage';
+// import { ProductPage } from './neha/ProductPage';
+import {SingleProduct} from "../src/harshal/SingleProduct"
 function App() {
   
   return (
     <div className="App">
       <Navbar/>
-      
+      <SingleProduct/>
     </div>
   );
 }
