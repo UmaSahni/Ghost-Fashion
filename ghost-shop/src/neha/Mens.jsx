ebfe8bb4fe7b1094922f3a3cfb78905de4ae80b6
unknown <1802.nehasahu@gmail.com>
2023-03-30 18:59:43 +0530

    day3

---
+++ ghost-shop/src/neha/Mens.jsx
@@ -0,0 +1,169 @@
+
+import { Box, Grid, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import Carousel from './Crousel';
+import collectionmens1 from "./images/collectionmens1.webp"
+import collectionmens3 from "./images/collectionmens3.webp"
+import collectionmens2 from "./images/collectionmens2.webp"
+import menscat1 from "./images/menscat1.webp"
+import menscat2 from "./images/menscat2.webp"
+import menscat3 from "./images/menscat3.webp"
+import { motion } from "framer-motion"
+import mmenscard1 from "./images/menscard1.webp"
+import mmenscard2 from "./images/menscard2.webp"
+import mmenscard3 from "./images/menscard3.webp"
+import mmenscard4 from "./images/menscard4.webp"
+import mmenscard5 from "./images/menscard5.webp"
+import mmenscard6 from "./images/menscard6.webp"
+import mmenscard7 from "./images/menscard7.webp"
+import mmenscard8 from "./images/menscard8.webp"
+import mmenscard9 from "./images/menscard9.webp"
+import mmenscard10 from "./images/menscard10.webp"
+import mmenscard11 from "./images/menscard11.webp"
+import mmenscard12 from "./images/menscard12.webp"
+import topsel1 from "./images/topsel1.webp"
+import topsell2 from "./images/topsell2.webp"
+import topsell3 from "./images/topsell3.webp"
+import topsell4 from "./images/topsell4.webp"
+import lastcrousel from "./images/lastcrousel.webp"
+import { Link } from 'react-router-dom';
+
+const Mens = () => {
+  return (
+    <Stack style={{marginTop:"-150px"}}
+    w={"90%"}
+    margin={"auto"}
+    >
+         <div className='crousel' >
+            <div>
+            <Carousel/>
+
+            </div>
+            <div className='collectionmens' style={{marginTop:"30px"}}>
+             <Box>
+                 <Text fontWeight={"bold"} color="gray" fontFamily={"heading"} fontSize="2xl">COLLECTIONS</Text>
+               <Grid w={"97%"} gridTemplateColumns={{base:"repeat(1,1fr)", md:"repeat(2,1fr)" , lg:"repeat(3,1fr)"}} gap="30px" margin={"auto"} marginTop={10}>
+                  <Image src={collectionmens1} alt="collection1"/>
+                  <Image src={collectionmens3} alt="collection2"/>
+                  <Image src={collectionmens2} alt="collection3"/>
+               </Grid>
+
+             </Box>
+
+            </div>
+            <motion.div   
+           
+            className='categorymens' style={{marginTop:"30px"}}>
+             <Box>
+                 <Text fontWeight={"bold"} color="gray" fontFamily={"heading"} fontSize="2xl">CATEGORIES</Text>
+               <Grid w={"97%"} gridTemplateColumns={{base:"repeat(1,1fr)", md:"repeat(2,1fr)" , lg:"repeat(3,1fr)"}} gap="30px" margin={"auto"} marginTop={10}>
+                  <motion.Box
+                  whileHover={{ scale: 1.02 }}
+                  whileTap={{ scale: 0.9 }}
+                  transition={{ type: "spring", stiffness: 400, damping: 50 }}
+                  >
+                  <Image 
+                  src={menscat1} alt="collection1"/>
+                  </motion.Box>
+                  
+                  <motion.Box
+                    whileHover={{ scale: 1.02 }}
+                    whileTap={{ scale: 0.9 }}
+                    transition={{ type: "spring", stiffness: 400, damping: 50 }}
+                  >
+                  <Image 
+                  src={menscat2} alt="collection1"/>
+                  </motion.Box>
+                  <motion.Box
+                   whileHover={{ scale: 1.02 }}
+                   whileTap={{ scale: 0.9 }}
+                   transition={{ type: "spring", stiffness: 400, damping: 50 }}
+                  >
+                  <Image 
+                  src={menscat3} alt="collection1"/>
+                  </motion.Box>
+               </Grid>
+
+             </Box>
+
+            </motion.div>
+        </div>
+
+
+         <div className='mensCards' style={{marginTop:"30px"}}>
+           <Grid gridTemplateColumns={{base:"repeat(2,1fr)", md:"repeat(3,1fr)" , lg:"repeat(4,1fr)"}} width="97%" margin={"auto"} gap={10}>
+
+            <Link to={"/mensproducts"}><Image src={mmenscard1} alt="mendscard" /></Link>
+            <Image src={mmenscard2}  alt="mendscard"/>
+            <Image src={mmenscard3} alt="mendscard"/>
+            <Image src={mmenscard4} alt="mendscard"/>
+            <Image src={mmenscard5} alt="mendscard"/>
+            <Image src={mmenscard6} alt="mendscard"/>
+            <Image src={mmenscard7} h={343} alt="mendscard"/>
+            <Image src={mmenscard8} alt="mendscard"/>
+            <Image src={mmenscard9} alt="mendscard"/>
+            <Image src={mmenscard10} alt="mendscard"/>
+            <Image src={mmenscard11} alt="mendscard"/>
+            <Image src={mmenscard12} w={"100%"} h={343} alt="mendscard"/>
+            
+
+           </Grid>
+
+         </div>
+
+         <div className='OFFICIAL MERCHANDISE' style={{marginTop:"30px"}}>
+         <Text fontWeight={"bold"} color="gray" fontFamily={"heading"} fontSize="2xl">OFFICIAL MERCHANDISE</Text>
+             
+             <Grid gridTemplateColumns={{base:"repeat(2,1fr)", md:"repeat(3,1fr)" , lg:"repeat(5,1fr)"}} gap={30} width="92%" margin="auto" marginTop={5}>
+                 <motion.Box
+                   whileHover={{ scale: 1.01 }}
+                   whileTap={{ scale: 0.9 }}
+                   transition={{ type: "spring", stiffness: 100, damping: 10 }}
+                 ><Image src='https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/Merchandies-images/web-icon.jpg?format=webp&w=300&dpr=1.3' alt='marvel'/></motion.Box>
+                
+                <motion.Box
+                   whileHover={{ scale: 1.01 }}
+                   whileTap={{ scale: 0.9 }}
+                   transition={{ type: "spring", stiffness: 100, damping: 10 }}
+                 ><Image src='https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/Merchandies-images/Marvel_Logo_268x220_b16y8nC_GKd3vRA.jpg?format=webp&w=300&dpr=1.3' alt='marvel'/></motion.Box>
+                
+                <motion.Box
+                   whileHover={{ scale: 1.01 }}
+                   whileTap={{ scale: 0.9 }}
+                   transition={{ type: "spring", stiffness: 100, damping: 10 }}
+                 ><Image src='https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/Merchandies-images/HarryPotter_Logo_268x220_LqBAKZU_Y8BG6uT.jpg?format=webp&w=300&dpr=1.3' alt='marvel'/></motion.Box>
+                
+                <motion.Box
+                   whileHover={{ scale: 1.01 }}
+                   whileTap={{ scale: 0.9 }}
+                   transition={{ type: "spring", stiffness: 100, damping: 10 }}
+                 ><Image src='https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/Merchandies-images/Disnety_Logo_268x220_GevoL2M_2j5PP2v.jpg?format=webp&w=300&dpr=1.3' alt='marvel'/></motion.Box>
+                 
+                 <motion.Box
+                   whileHover={{ scale: 1.01 }}
+                   whileTap={{ scale: 0.9 }}
+                   transition={{ type: "spring", stiffness: 100, damping: 10 }}
+                 ><Image src='https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/Merchandies-images/DC_Logo_268x220_qtYfouo_VyuqmVD.jpg?format=webp&w=300&dpr=1.3' alt='marvel'/></motion.Box>
+
+             </Grid>
+
+         </div>
+               <div className='topselling' style={{marginTop:"30px"}}>
+               <Text fontWeight={"bold"} color="gray" fontFamily={"heading"} fontSize="2xl">TOP SELLING</Text>
+                 <Grid gridTemplateColumns={{base:"repeat(2,1fr)", md:"repeat(2,1fr)" , lg:"repeat(4,1fr)"}} gap={30} w={"95%"} margin="auto" marginTop={7}>
+                  <Image src={topsel1} alt="topsell1"/>
+                  <Image src={topsell2} alt="topsell"/>
+                  <Image src={topsell3} alt="topsell"/>
+                  <Image src={topsell4} alt="topsell"/>
+                 </Grid>
+               </div>
+
+
+               <div className='lastcrousel' style={{marginTop:"30px"}} >
+                <Image src={lastcrousel} alt='last' w={"95%"} margin="auto"/>
+               </div>
+    </Stack>
+  );
+}
+
+export default Mens;
---
+++ ghost-shop/src/neha/WomensCrousel.jsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import { Box, IconButton, useBreakpointValue } from '@chakra-ui/react';
+// Here we have used react-icons package for the icons
+import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
+// And react-slick as our Carousel Lib
+import Slider from 'react-slick';
+
+import womenscrousel1 from "./images/womenscrousel1.webp"
+import womenscrousel2 from "./images/womenscrousel2.webp"
+import womenscrousel3 from "./images/womenscrousel3.webp"
+import womenscrousel4 from "./images/womenscrousel4.webp"
+// Settings for the slider
+const settings = {
+  dots: true,
+  arrows: false,
+  fade: true,
+  infinite: true,
+  autoplay: true,
+  speed: 500,
+  autoplaySpeed: 5000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+export default function WomensCarousel() {
+  // As we have used custom buttons, we need a reference variable to
+  // change the state
+  const [slider, setSlider] = React.useState(null);
+
+  // These are the breakpoints which changes the position of the
+  // buttons as the screen size changes
+  const top = useBreakpointValue({ base: '90%', md: '50%' });
+  const side = useBreakpointValue({ base: '30%', md: '10px' });
+
+;
+
+  return (
+    <Box
+      position={'relative'}
+      height={'500px'}
+      
+      overflow={'hidden'}
+      margin="auto"
+      marginTop={150}
+      width={{base:"80%" , sm:"90%" , md:"90%" , lg:'98%'}}
+      >
+      
+      {/* CSS files for react-slick */}
+      <link
+        rel="stylesheet"
+        type="text/css"
+        charSet="UTF-8"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+      />
+      {/* Left Icon */}
+      <IconButton
+        aria-label="left-arrow"
+        colorScheme="messenger"
+        borderRadius="full"
+        position="absolute"
+        left={side}
+        top={top}
+        transform={'translate(0%, -50%)'}
+        zIndex={2}
+        onClick={() => slider?.slickPrev()}>
+        <BiLeftArrowAlt />
+      </IconButton>
+      {/* Right Icon */}
+      <IconButton
+        aria-label="right-arrow"
+        colorScheme="messenger"
+        borderRadius="full"
+        position="absolute"
+        right={side}
+        top={top}
+        transform={'translate(0%, -50%)'}
+        zIndex={2}
+        onClick={() => slider?.slickNext()}>
+        <BiRightArrowAlt />
+      </IconButton>
+      {/* Slider */}
+      <Slider {...settings} ref={(slider) => setSlider(slider)}>
+       
+          <Box
+            
+            height={'xl'}
+            position="relative"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+            backgroundSize="cover"
+            backgroundImage={womenscrousel1}
+            marginTop="20px"
+          />
+           <Box
+            
+            height={'xl'}
+            position="relative"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+            backgroundSize="cover"
+            backgroundImage={womenscrousel3}
+          />
+             <Box
+            
+            height={'xl'}
+            position="relative"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+            backgroundSize="cover"
+            backgroundImage={womenscrousel2}
+          />
+           <Box
+            
+            height={'xl'}
+            position="relative"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+            backgroundSize="cover"
+            backgroundImage={womenscrousel4}
+          />
+      </Slider>
+    </Box>
+  );
+}
