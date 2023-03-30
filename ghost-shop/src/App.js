e578d9c090425230cca7bd29ab2091712abb6d09
VinitAsutkar <vinit123asutkar@gmail.com>
2023-03-30 19:42:37 +0530

    Footer

--- ghost-shop/src/App.js
+++ ghost-shop/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import { ProductPage } from './neha/ProductPage';
 function App() {
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      
+      <Footer/>
     </div>
   );
 }
