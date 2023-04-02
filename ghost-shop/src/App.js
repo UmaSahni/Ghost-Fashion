d053904d12c8418dc5ac78c22a190a3c2bb20b33
harshal-kitukale <harshalkitukale005@gmail.com>
2023-04-02 20:45:26 +0530

    added functionality

--- ghost-shop/src/App.js
+++ ghost-shop/src/App.js
@@ -1,13 +1,13 @@
 import AllRoutes from './AllRoutes/AllRoutes';
 import './App.css';
-import Navbar from './Components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      
       <AllRoutes />
+      
     </div>
   );
 }
--- ghost-shop/src/Pages/Home.jsx
+++ ghost-shop/src/Pages/Home.jsx
@@ -1,8 +1,8 @@
 import { Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import Footer from '../Components/Footer'
 // import styled from 'styled-components'
 import Navbar from '../Components/Navbar'
-// import Footer from '../Components/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +11,7 @@ const Home = () => {
       <Image src='https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/home-landing-web/Homepage-banner-Ajio.jpg?w=1500&dpr=1.3'/>
       {/* <Text p={'1'} color={'white'} bg='#E71318' fontFamily={'sans-serif'} fontSize={['xl','2xl','3xl','4xl']}>HOMEGROWN INDIAN BRAND</Text>
       <Text p={'2'} fontSize={['xl','2xl','3xl','4xl','5xl']}>Over <Span>5.5 Million</Span> Happy Customers</Text> */}
+    <Footer/>
     </div>
   )
 }
